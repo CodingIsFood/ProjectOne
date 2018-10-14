6f73c21f580bdf544b1318ca387f9745dfcca1c2
Ayokunle <ayoodedere@gmail.com>
2018-10-14 13:07:01 +0100

    Hurray! Commited and Pushed from IDE, not terminal!

--- newfile.py
+++ newfile.py
@@ -2,6 +2,6 @@
 
 
 
-//This comment was added by user 2 from his computer
+//This comment was added by user 2 from his computer! Pushed from IDE, not terminal
 print ("My name is Ayokunle Odedere")
 
