0d0025987238c083f41b4105b2420249d634cc60
Ayokunle <ayoodedere@gmail.com>
2018-10-14 13:38:44 +0100

    changes by new branch

--- newfile.py
+++ newfile.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python/
 
 
-
+#comment is added from another branch
 //This comment was added by user 2 from his computer! Pushed from IDE, not terminal
 print ("My name is Ayokunle Odedere")
 
